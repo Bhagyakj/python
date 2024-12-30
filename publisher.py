000d1dac8e00afe6c41dddb498fac88ef9dfb192
Bhagyakj <bhagyalakshmikj02@gmail.com>
2024-12-30 14:35:07 +0530

    Add files via upload

--- publisher.py
+++ publisher.py
@@ -2,15 +2,25 @@ class Publisher:
     def __init__(self,name):
         self.name=name
     def display(self):
-        print(self.name)
+        print("publisher:",self.name)
 class Book(Publisher):
-    def __init__(self,name,title,author):
+    def __init__(self,name,title,auther):
         super().__init__(name)
         self.title=title
-        self.author=author
-        def display(self):
-            super().display()
-            print(self.title)
-            print(self.author)
+        self.auther=auther
+    def display(self):
+        super().display()
+        print("title:",self.title)
+        print("auther:",self.auther)
 class Python(Book):
-    def
+    def __init__(self,name,title,auther,price,noofpages):
+        super().__init__(name,title,auther)
+        self.price=price
+        self.noofpages=noofpages
+    def display(self):
+        super().display()
+        print("price:",self.price)
+        print("no of pages:",self.noofpages)
+p1=Python("DC Books","Agnichirakukal","APJ Abdul kalam",350,500)
+p1.display()
+
